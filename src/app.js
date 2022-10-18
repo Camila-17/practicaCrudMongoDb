542f753bf472809f05caafd47f31798fea429c52
Maria Camila <camilarxn023@gmail.com>
2022-10-17 21:50:30 -0500

    Intento de enlace con style

--- src/app.js
+++ src/app.js
@@ -1,29 +1,32 @@
-const path= require('path');
-const express = require('express');
 
-const morgan = require('morgan');
+const path = require("path");
+const express = require("express");
 
-const mongoose = require('mongoose');
+const morgan = require("morgan");
+
+const mongoose = require("mongoose");
 
 const app = express();
+
+app.use(express.static('public'));
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/dbUsuario")
   .then((db) => console.log("Database MongoDB - dbUsuario connected"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
-const indexRut = require('./routes/index');
+const indexRut = require("./routes/index");
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname,'views'));
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded());
 app.use("/", indexRut); //ERROR EN LINEA 24
 
-app.listen(app.get('port'), () => {
-    console.log(`Server on port ${app.get('port')}`);
+app.listen(app.get("port"), () => {
+  console.log(`Server on port ${app.get("port")}`);
 });
 // console.log('Hola mundo')
